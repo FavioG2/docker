d7ba56cf18007a2ac779bec3ebf382250fc7fa7d
Favio <faviog2@gmail.com>
2018-09-05 09:09:41 -0600

    trabajando

--- python/flaskapp/helloworld.py
+++ python/flaskapp/helloworld.py
@@ -1,19 +1,17 @@
-from flask import Flask, request, render_template
-import redis
-import sys
-
+from flask import Flask, jsonify, request
 app = Flask(__name__)
-r = redis.StrictRedis(host= 'redis', port=6379, db=0)
-app.add_url_rule(app.static_url_path + '/<path:filename>',
-                 endpoint='http://reverseproxy:8080',
-                 view_func=app.send_static_file,
-                 host='http://reverseproxy:8080')
-app.url_map.host_matching = True
 
-@app.route('/')
-def index():
-    ##print(app.static_url_path)
-    return render_template('form.html')
+empDB=[
+ {
+ 'id':'101',
+ 'name':'Saravanan S',
+ 'title':'Technical Leader'
+ }
+ ]
+ 
+@app.route("/api", methods=['GET'])
+def hello():
+    return jsonify({'emps':empDB})
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -27,6 +25,6 @@ def leer():
     nombre = request.form['producto']
     return r.get(nombre)
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
